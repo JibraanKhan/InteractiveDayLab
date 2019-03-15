69ba0f516a79155fce27ce7ffb5457e88b7cd5a9
KingLoneCat <jibraankhan@KingLoneCats-MacBook-Air.local>
2019-03-14 22:14:18 -0400

    At least it works.

--- main.js
+++ main.js
@@ -53,8 +53,8 @@ var initialize = function(data, day) {
         .enter()
         .append('g')
         .classed('student', true)
-        .attr('fill', function(d){
-          return color(d.name);
+        .attr('fill', function(d) {
+            return color(d.name);
         })
     var xScale = d3.scaleBand()
         .domain(d3.range(current_dataset.grades.length))
@@ -91,100 +91,102 @@ var initialize = function(data, day) {
             return heightScale(d.grade);
         })
         .attr('width', xScale.bandwidth())
-        .on('mouseover', function(d, i){
-          var rect = d3.select(this);
-          var rect_parent = d3.select(this.parentNode);
-          var grade = d.grade;
-          var tooltip_width = xScale.bandwidth() + 20
-          var name = d.name;
-          var mouse_pos = d3.mouse(this);
-          var m_x = mouse_pos[0];
-          var m_y = mouse_pos[1];
-          var tooltip_height = heightScale(25)
-          rect.attr('height', heightScale(grade + 1))
-              .attr('y', h - rect.attr('height') + margins.top)
+        .on('mouseover', function(d, i) {
+            var rect = d3.select(this);
+            var rect_parent = d3.select(this.parentNode);
+            var grade = d.grade;
+            var tooltip_width = xScale.bandwidth() + 20
+            var name = d.name;
+            var mouse_pos = d3.mouse(this);
+            var m_x = mouse_pos[0];
+            var m_y = mouse_pos[1];
+            var tooltip_height = heightScale(25)
+            rect.attr('height', heightScale(grade + 1))
+                .attr('y', h - rect.attr('height') + margins.top)
 
-          var starting_y;
+            var starting_y;
 
-          if ((Math.abs(m_y - scr.h)) < (m_y)){
-            starting_y = scr.h + tooltip_height;
-          } else{
-            starting_y = -tooltip_height;
-          }
-          this.parentNode.parentNode.appendChild(this.parentNode)
-          // this.parentNode.appendChild(this);
-          var tooltip = rect_parent.append('g')
-                                   .classed('tooltip', true)
-          var base_tip = tooltip.append('rect')
-                             .attr('x', m_x)// rect.attr('x') - tooltip_width/2
-                             .attr('y', starting_y)
-                             .attr('width', tooltip_width)
-                             .attr('height', tooltip_height)
-                             .attr('fill', '#c17b30')
-                             .style('pointer-events', 'none')
-                             .classed('Tooltip', true)
-                             .attr('opacity', 0)
-                             .style('z-index', 10);
+            if ((Math.abs(m_y - scr.h)) < (m_y)) {
+                starting_y = scr.h + tooltip_height;
+            } else {
+                starting_y = -tooltip_height;
+            }
+            this.parentNode.parentNode.appendChild(this.parentNode)
+            // this.parentNode.appendChild(this);
+            var tooltip = rect_parent.append('g')
+                .classed('tooltip', true)
+            var base_tip = tooltip.append('rect')
+                .attr('x', m_x) // rect.attr('x') - tooltip_width/2
+                .attr('y', starting_y)
+                .attr('width', tooltip_width)
+                .attr('height', tooltip_height)
+                .attr('fill', '#c17b30')
+                .style('pointer-events', 'none')
+                .classed('Tooltip', true)
+                .attr('opacity', 0)
+                .style('z-index', 10);
 
-          var texts = ['Student: ' + name,
-                       'Grade: ' + grade
-                      ]
-          var txt_tip = tooltip.selectAll('text')
-                               .data(texts)
-                               .enter()
-                               .append('text')
-                               .attr('x', m_x)//rect.attr('x') - tooltip_width/2
-                               .attr('stroke', 'black')
-                               .attr('y', starting_y)// m_y + (tooltip_height)/2
-                               .text(function(d){ return d; })
-                               .style('pointer-events', 'none')
-                               .attr('opacity', 0)
-                               .classed('Info', true)
-          base_tip.transition()
-                  .attr('y', Math.min(m_y, h - (tooltip_height/2)))
-                  .attr('opacity', 1);
+            var texts = ['Student: ' + name,
+                'Grade: ' + grade
+            ]
+            var txt_tip = tooltip.selectAll('text')
+                .data(texts)
+                .enter()
+                .append('text')
+                .attr('x', m_x) //rect.attr('x') - tooltip_width/2
+                .attr('stroke', 'black')
+                .attr('y', starting_y) // m_y + (tooltip_height)/2
+                .text(function(d) {
+                    return d;
+                })
+                .style('pointer-events', 'none')
+                .attr('opacity', 0)
+                .classed('Info', true)
+            base_tip.transition()
+                .attr('y', Math.min(m_y, h - (tooltip_height / 2)))
+                .attr('opacity', 1);
 
-          txt_tip.transition()
-                 .attr('y', function(d, i){
-                   return Math.min((m_y + (tooltip_height)/texts.length * (i + 0.5)), (h - (tooltip_height/2) + (tooltip_height)/texts.length * (i + 0.5)));
-                 })
-                 .attr('opacity', 1)
+            txt_tip.transition()
+                .attr('y', function(d, i) {
+                    return Math.min((m_y + (tooltip_height) / texts.length * (i + 0.5)), (h - (tooltip_height / 2) + (tooltip_height) / texts.length * (i + 0.5)));
+                })
+                .attr('opacity', 1)
 
         })
-        .on('mouseout', function(d, i){
-          var rect = d3.select(this);
-          var rect_parent = d3.select(this.parentNode);
-          var tooltip = rect_parent.selectAll('g');
-          var mouse_pos = d3.mouse(this);
-          var m_x = mouse_pos[0];
-          var m_y = mouse_pos[1];
+        .on('mouseout', function(d, i) {
+            var rect = d3.select(this);
+            var rect_parent = d3.select(this.parentNode);
+            var tooltip = rect_parent.selectAll('g');
+            var mouse_pos = d3.mouse(this);
+            var m_x = mouse_pos[0];
+            var m_y = mouse_pos[1];
 
-          rect.attr('height', heightScale(d.grade - 1))
-              .attr('y', h - rect.attr('height') + margins.top)
+            rect.attr('height', heightScale(d.grade - 1))
+                .attr('y', h - rect.attr('height') + margins.top)
 
-          var ending_y;
-          var base = tooltip.select('.Tooltip')
-          var texts = tooltip.selectAll('.Info');
-          var tooltip_height = heightScale(25)
-          var tooltip_width = xScale.bandwidth() + 20
+            var ending_y;
+            var base = tooltip.select('.Tooltip')
+            var texts = tooltip.selectAll('.Info');
+            var tooltip_height = heightScale(25)
+            var tooltip_width = xScale.bandwidth() + 20
 
-          if ((Math.abs(m_y - scr.h)) < (m_y)){
-            ending_y = scr.h + tooltip_height;
-          } else{
-            ending_y = -tooltip_height;
-          }
+            if ((Math.abs(m_y - scr.h)) < (m_y)) {
+                ending_y = scr.h + tooltip_height;
+            } else {
+                ending_y = -tooltip_height;
+            }
 
-          console.log("Got through")
-          base.transition()
-                 .attr('y', ending_y)
-                 .attr('opacity', 0)
-                 .on('end', function(){
-                   tooltip.remove();
-                 });
+            console.log("Got through")
+            base.transition()
+                .attr('y', ending_y)
+                .attr('opacity', 0)
+                .on('end', function() {
+                    tooltip.remove();
+                });
 
-          texts.transition()
-               .attr('y', ending_y)
-               .attr('opacity', 0)
+            texts.transition()
+                .attr('y', ending_y)
+                .attr('opacity', 0)
         })
 
 
@@ -201,13 +203,13 @@ var initialize = function(data, day) {
     // Create Buttons
     var img_data = ['images/UpArrow.png', 'images/DownArrow.png'];
     var button_svg_dims = {
-      width:200,
-      height: scr.h
+        width: 200,
+        height: scr.h
     }
     var buttons_svg = d3.select('body').append('svg')
-                                       .attr('width', button_svg_dims.width)
-                                       .attr('height', button_svg_dims.height)
-                                       .classed('ButtonHolder', true);
+        .attr('width', button_svg_dims.width)
+        .attr('height', button_svg_dims.height)
+        .classed('ButtonHolder', true);
     var buttons = buttons_svg.selectAll('image')
         .data(img_data)
         .enter()
@@ -215,35 +217,34 @@ var initialize = function(data, day) {
         .attr('xlink:href', function(d) {
             return d;
         })
-        .attr('class', function(d,i){
-          return 'button' + i;
+        .attr('class', function(d, i) {
+            return 'button' + i;
         }, true)
         .attr('width', '100%')
         .attr('height', '50%')
-        .attr('y', function(d, i){
-          return i * (button_svg_dims.height/3);
+        .attr('y', function(d, i) {
+            return i * (button_svg_dims.height / 3);
         })
-        .on('click', function(){
-          if (this.getAttribute('class') == 'button0'){
-            if (day < data.length - 1){
-              day++;
-              centralText = update(data[day], students, heightScale, h, margins, buttons, centralText, day, buttons_svg, button_svg_dims, 'up');
+        .on('click', function() {
+            if (this.getAttribute('class') == 'button0') {
+                if (day < data.length - 1) {
+                    day++;
+                    centralText = update(data[day], students, heightScale, h, margins, buttons, centralText, day, buttons_svg, button_svg_dims, 'up');
+                }
+            } else {
+                if (day > 1) {
+                    day--;
+                    centralText = update(data[day], students, heightScale, h, margins, buttons, centralText, day, buttons_svg, button_svg_dims, 'down');
+                }
             }
-          }
-          else{
-            if (day > 1){
-              day--;
-              centralText = update(data[day], students, heightScale, h, margins, buttons, centralText, day, buttons_svg, button_svg_dims, 'down');
-            }
-          }
         })
 
     var centralText = buttons_svg.append('text')
-                                 .attr('y', button_svg_dims.height * 0.45)
-                                 .text('Day: ' + day)
-                                 .attr('x', button_svg_dims.width * 0.25)
-                                 .attr('stroke', 'lightblue')
-                                 .attr('font-size', 40)
+        .attr('y', button_svg_dims.height * 0.45)
+        .text('Day: ' + day)
+        .attr('x', button_svg_dims.width * 0.25)
+        .attr('stroke', 'lightblue')
+        .attr('font-size', 40)
 
 }
 
@@ -255,54 +256,54 @@ var initialize = function(data, day) {
 // position:absolute;
 // left: 40%;
 // top: 27%;
-var update = function(data, students, heightScale, h, margins, buttons, label, day, label_parent, svg_dims, dir){
-  students.data(data.grades).enter();
-   students.select('rect')
-           .transition()
-           .attr('height', function(d){
-             return heightScale(d.grade);
-           })
-           .attr('y', function(d){
-             return h - heightScale(d.grade) + margins.top;
-           })
+var update = function(data, students, heightScale, h, margins, buttons, label, day, label_parent, svg_dims, dir) {
+    students.data(data.grades).enter();
+    students.select('rect')
+        .transition()
+        .attr('height', function(d) {
+            return heightScale(d.grade);
+        })
+        .attr('y', function(d) {
+            return h - heightScale(d.grade) + margins.top;
+        })
 
 
-   var starting_y;
+    var starting_y;
 
-   if (dir == 'up'){
-     label.transition()
-          .duration(1000)
-          .attr('y', 0)
-          .attr('opacity', 0)
-          .on('end', function(d, i){
-            d3.select(this).remove();
-          })
-     starting_y = svg_dims.height * 2;
-   }else{
-     label.transition()
-          .duration(1000)
-          .attr('y', svg_dims.height)
-          .attr('opacity', 0)
-          .on('end', function(d, i){
-            d3.select(this).remove();
-          })
-     starting_y = -30;
-   }
-
-
-   var new_label = label_parent.append('text')
-                               .attr('y', starting_y)
-                               .text('Day: ' + day)
-                               .attr('x', svg_dims.width * 0.25)
-                               .attr('stroke', 'lightblue')
-                               .attr('font-size', 40)
-
-  new_label.transition()
-           .duration(1000)
-           .attr('y', svg_dims.height * 0.45)
-           .attr('opacity', 1)
+    if (dir == 'up') {
+        label.transition()
+            .duration(1000)
+            .attr('y', 0)
+            .attr('opacity', 0)
+            .on('end', function(d, i) {
+                d3.select(this).remove();
+            })
+        starting_y = svg_dims.height * 2;
+    } else {
+        label.transition()
+            .duration(1000)
+            .attr('y', svg_dims.height)
+            .attr('opacity', 0)
+            .on('end', function(d, i) {
+                d3.select(this).remove();
+            })
+        starting_y = -30;
+    }
 
 
+    var new_label = label_parent.append('text')
+        .attr('y', starting_y)
+        .text('Day: ' + day)
+        .attr('x', svg_dims.width * 0.25)
+        .attr('stroke', 'lightblue')
+        .attr('font-size', 40)
 
-  return new_label;
+    new_label.transition()
+        .duration(1000)
+        .attr('y', svg_dims.height * 0.45)
+        .attr('opacity', 1)
+
+
+
+    return new_label;
 }
